f716936a1bea48fbad83c325722cbf11f681e6e0
Risav03 <needleweed03@gmail.com>
2024-02-06 23:14:39 +0530

    init

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Rainbow from "@/utils/rainbow/rainbowKit";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Rainbow>
+          {children}
+        </Rainbow>
+        </body>
     </html>
   );
 }
---
+++ components/UI/burner.jsx
@@ -0,0 +1,11 @@
+"use client"
+
+import {WalletConnectButton} from "../Buttons/walletConnectButton"
+
+export default function Burner(){
+    return(
+        <div>
+            <WalletConnectButton/>
+        </div>
+    )
+}
