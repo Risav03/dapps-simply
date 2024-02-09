94eb806799a23efd099d5f7b4b76b7fd25ef6c1a
Risav03 <needleweed03@gmail.com>
2024-02-09 12:22:46 +0530

    check now

--- utils/contractAdds.jsx
+++ utils/contractAdds.jsx
@@ -1,8 +1,8 @@
 "use client"
 
 export const contractAdds = {
-    burner: "0xbFFa5d24E38422934cA70eF5dc96d030E4B70607",
+    burner: "0xCA4DAD49212746D25fe885cF768A8f0050A9dc4C",
     simple: "0x628a73c527f1c191F27C4d466238381109B6eAFC",
-    caller: "0x72d79a66dc03dFB4623bc49F3Bfb836165277cDf",
+    caller: "0xdCA1c5D12B53393Ade1690445E1FdEd5e4AAD589",
     simplyNFT: "0x1a90102b680807CE176bE575479e4b824fD1F392"
 }
