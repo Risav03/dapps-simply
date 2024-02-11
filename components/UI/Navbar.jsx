240b3e009ca1d148a109d382f959e9882f0ed7bb
Risav03 <needleweed03@gmail.com>
2024-02-12 02:18:43 +0530

    gray scaled

--- components/UI/Navbar.jsx
+++ components/UI/Navbar.jsx
@@ -64,9 +64,9 @@ export default function Navbar(){
 
     return(
         <div className="flex flex-row sm:items-end sm:justify-end sm:mt-2 mt-5 mb-5 items-center justify-center relative">
-            <div className="flex items-center w-[14rem] text-center justify-center -translate-y-3 -translate-x-2">
-                <Image width={1920} height={1080} src={tokenBox} className="absolute w-[14rem] hue-rotate-[45deg]"/>
-                <h2 className="translate-y-2 text-center text-xl flex items-center font-bold text-white">{balance} <span ><Image src={token} width={1920} height={1080} className="w-[3.2rem] translate-x-4 -rotate-12" /></span></h2>
+            <div className="flex items-center w-[14rem] text-center  justify-center -translate-y-3 -translate-x-2">
+                <Image width={1920} height={1080} src={tokenBox} className="absolute w-[14rem] grayscale brightness-150"/>
+                <h2 className="translate-y-2 text-center text-xl flex items-center font-bold text-black">{balance} <span ><Image src={token} width={1920} height={1080} className="w-[2.5rem] translate-x-4 -rotate-12" /></span></h2>
             </div>
             <WalletConnectButton/>
         </div>
