79ea1c3bdc12e10b8b62ff618f75126bd3daa9dc
Risav03 <needleweed03@gmail.com>
2024-02-22 13:36:10 +0530

    check

---
+++ app/marketplace/page.js
@@ -0,0 +1,12 @@
+import Burner from "@/components/UI/burner";
+import Image from "next/image";
+import bg from "@/assets/background.jpg"
+
+export default function Home() {
+  return (
+    <main className="text-white bg-gradient-to-b from-black/10 to-black flex h-[100vh] items-center justify-center">
+      <Image src={bg} width={1920} height={1080} className="w-screen h-screen absolute top-0 left-0 z-[-2]" />
+      <h1 className="font-bold text-5xl animate-pulse">BEING BUILT... 🛠️</h1>
+    </main>
+  );
+}
