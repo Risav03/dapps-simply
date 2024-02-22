2748f64956c0cf3616020c5dc9368533abb7da98
Risav03 <needleweed03@gmail.com>
2024-02-23 00:02:29 +0530

    text to loader

--- components/UI/raffleCards.jsx
+++ components/UI/raffleCards.jsx
@@ -288,7 +288,7 @@ export default function RaffleFetcher({number}){
     return(
         <div className="w-[20rem] relative h-fit text-center">
             <div className="bg-blue-500 z-[-1] top-2 left-2 absolute h-full w-full"></div>
-            {loadingRaffle &&<div className="mx-auto flex items-center justify-center"> <InfinitySpin className="translate-x-10" visible={true} width="200" color="#ffffff" ariaLabel="infinity-spin-loading" /></div>}
+            {loadingRaffle &&<div className="mx-auto flex items-center justify-center"> <InfinitySpin className="translate-x-10" visible={true} width="200" color="#ffffff" ariaLabel="infinity-spin-loading" /><h1>Fetching data...</h1></div>}
             {itemExists ? <div className="bg-yellow-400 w-full p-5 mx-auto">
                 <Image width={1920} height={1080} className="w-full bg-white min-[1500px]:w-[90%] mx-auto border-2 border-black" src={image}/>
                 <h2 className="text-2xl bg-yellow-400 text-black w-fit mx-auto px-4 py-2 my-2">{name}</h2>
