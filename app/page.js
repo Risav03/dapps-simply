8911fde9603e6ec80c6164e0beda0178bc575a0f
Risav03 <needleweed03@gmail.com>
2024-02-22 16:26:41 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import bg from "@/assets/background.jpg"
 
 export default function Home() {
   return (
-    <main className="text-white text-center px-20 py-36">
+    <main className="text-white text-center px-8 py-36">
       <Image src={bg} width={1920} height={1080} className="w-screen h-screen fixed top-0 left-0 z-[-2]" />
       
       <h1 className="text-7xl font-bold">SIMPLY <span className="bg-gradient-to-b from-green-400 to-green-700 text-transparent bg-clip-text">dApps</span></h1>
@@ -15,21 +15,21 @@ export default function Home() {
       <div className="w-[25rem] relative  group">
         <div className="absolute w-full h-full bg-gradient-to-b from-orange-400 top-0 right-0 group-hover:shadow-2xl group-hover:shadow-orange-500/60 to-orange-600 duration-200 z-[-1] group-hover:top-2 group-hover:right-2"></div>
         <Link href="/burner">
-            <button className="text-3xl w-full font-bold bg-black h-[15rem]">Burner</button>
+            <button className="text-3xl w-full font-bold bg-black h-[12rem]">Burner</button>
         </Link>
       </div>
 
       <div className="w-[25rem] relative group">
       <div className="absolute w-full h-full bg-gradient-to-b from-blue-400 top-0 group-hover:shadow-2xl group-hover:shadow-blue-500/50 to-blue-600 duration-200 z-[-1] group-hover:top-2 "></div>
       <Link href="/raffle">
-            <button className="text-3xl w-full font-bold bg-black h-[15rem]">Raffle</button>
+            <button className="text-3xl w-full font-bold bg-black h-[12rem]">Raffle</button>
         </Link>
       </div>
 
       <div className="w-[25rem] relative group">
         <div className="absolute w-full h-full bg-gradient-to-b from-green-400 top-0 left-0 group-hover:shadow-2xl group-hover:shadow-green-500/60 to-green-600 duration-200 z-[-1] group-hover:top-2 group-hover:left-2"></div>
         <Link href="/marketplace">
-            <button className="text-3xl w-full font-bold bg-black h-[15rem]">Marketplace</button>
+            <button className="text-3xl w-full font-bold bg-black h-[12rem]">Marketplace</button>
         </Link>      </div>
 
     </div>
--- components/UI/raffle.jsx
+++ components/UI/raffle.jsx
@@ -5,21 +5,25 @@ import Navbar from "./Navbar";
 import RaffleCards from "./raffleCards";
 import PastWinners from "./pastwinners";
 
+import {useAccount} from "wagmi";
+
 export default function Raffle(){
 
     const[type, setType] = useState("Simple");
 
+    const{address} = useAccount();
 
     return (
         <div className="p-10">
             <Navbar/>
-            <h1 className="text-5xl font-bold">Simple <span className="bg-gradient-to-b from-blue-400 to-blue-600 text-transparent bg-clip-text">Raffle dApp</span></h1>
+            <h1 className="text-6xl font-bold">SIMPLY <span className="bg-gradient-to-b from-blue-400 to-blue-600 text-transparent bg-clip-text">Raffle dApp</span></h1>
 
             <div className="mx-auto w-[20rem]  grid grid-flow-col grid-cols-2 px-1 py-3">
                 <button onClick={()=>{setType("Simple")}} className={`${ type == "Simple" ? "bg-white shadow-lg shadow-white/50 text-black": "bg-black text-white"} px-2 py-3  rounded-l-xl font-bold`}>$SIMPLE</button>
                 <button onClick={()=>{setType("Matic")}} className={`${ type == "Matic" ? "bg-purple-500 shadow-lg shadow-purple-400/50 text-white": "bg-black text-purple-500"}  rounded-r-xl px-2 py-3  font-bold`}>$MATIC</button>
             </div>
 
+            { address == "0x0708a59Ea3d6e8Dd1492fc2bBDC54A82905D9f59" && <a href="https://simply-admin.vercel.app/"><button className="bg-red-500 p-4 text-xl font-bold">Admin Dashboard</button></a>}
 
             <div className=" mt-5 ">
                 <h1 className="text-3xl font-bold mb-5">${type} Raffle</h1>
